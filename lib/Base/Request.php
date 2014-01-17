4e46a97da42a75f8d040745799e86e88ddd86c37
oskolskikh <oskolskikh@gmail.com>
2014-01-17 16:01:38 +0200

    Request

--- lib/Base/Request.php
+++ lib/Base/Request.php
@@ -7,5 +7,32 @@ namespace Base;
 
 class Request
 {
-  
+    public $server;
+    public $files;
+    public $data;
+    public $hasFiles =false;
+    
+    public function __construct()
+    {
+        
+	$this->server = new DataHolder($_SERVER);
+	$this->setFiles();
+        $this->data = new DataHolder($_REQUEST);
+    }
+    
+    public function setFiles(){
+        if (isset($_FILES) && !empty($_FILES))
+        {
+            $this->files = new DataHolder($_FILES);
+            $this->hasFiles = true;
+        }else{
+            $this->hasFiles = false;
+        }
+            
+    }
+    
+    public function method()
+    {
+       return $this->server->get('REQUEST_METHOD', 'GET');
+    }
 }
